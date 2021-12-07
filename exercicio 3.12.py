d3fc1db7f336f3abfd01c8a0c7efe1cff038d0ea
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-07 10:39:55 -0300

    Exercício 3.12
    Escreva um programa que calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.

---
+++ exercicio 3.12.py
@@ -0,0 +1,4 @@
+distancia = float(input("Qual a distância a percorrer em metros da viagem? "))
+vmedia = float(input("Qual a velocidade média esperada para a viagem em m/s: "))
+tempo = distancia/vmedia
+print ("O tempo médio da viagem será de %5.2f segundos." % (distancia/vmedia))
