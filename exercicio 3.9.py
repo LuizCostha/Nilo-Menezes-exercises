f6ebad8af9eea9822946dd4917ce2d19d9b27fbb
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-07 10:20:21 -0300

    Exercício 3.9
    Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.

---
+++ exercicio 3.9.py
@@ -0,0 +1,7 @@
+dias = int(input("Digite a quantidade de dias: "))
+horas = int(input("Digite a quantidade de horas: "))
+minutos = int(input("Digite a quantidade de minutos: "))
+segundos = int(input("Digite a quantidade de segundos: "))
+print ("O número total de segundos é %d." % ((dias * 24 * 60 * 60) + (horas * 60 * 60) + (minutos * 60) + segundos))
+
+
