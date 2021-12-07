b579a2660517670d848db15c2ce9deb29519153c
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-07 10:17:13 -0300

    Exercício 3.6
    Escreva uma expressão que será utilizada para decidir se um aluno foi ou não aprovado. Para ser aprovado, todas as médias do aluno devem ser maiores que 7. Considere que o aluno cursa apenas três matérias e que a nota de cada uma está armazenada nas seguintes variáveis: matéria1, matéria2 e matéria3.

---
+++ exercicio 3.6.py
@@ -0,0 +1,8 @@
+materia1 = float(input("Digite sua nota na materia 1: "))
+materia2 = float(input("Digite sua nota na materia 2: "))
+materia3 = float(input("Digite sua nota na materia 3: "))
+if materia1 >= 7 and materia2 >= 7 and materia3 >= 7:
+    print("Você foi aprovado no curso.")
+else:
+    print("Você foi reprovado.")
+
