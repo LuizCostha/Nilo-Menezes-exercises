1c6bc621d5cd572fdb973e04775b5f8377c17e2a
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-08 08:49:43 -0300

    Exercício 3.15
    Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 10 minutos de vida a cada cigarro, calcule quantos dias de vida um fumante perderá. Exiba o total em dias.

---
+++ exercicio 3.15.py
@@ -0,0 +1,13 @@
+ciga = int(input("Qual o número de cigarros fumados por dia? "))
+anos = int(input("Há quantos anos você fuma cigarro? "))
+totalcigarros = ciga * anos * 365
+tempoperdido = totalcigarros * 10 / 60 / 24
+print ("O total de dias perdidos em razão do vício do cigarros é %3.2f dias." % tempoperdido)
+
+
+
+
+
+
+
+
