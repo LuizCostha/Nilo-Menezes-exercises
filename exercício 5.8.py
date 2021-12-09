b05c529c5cb440a120611a3ba1a71d64105ddd08
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-09 09:07:21 -0300

    Exercício 5.8
    Escreva um programa que leia dois números. Imprima o resultado da multiplicação do primeiro pelo segundo. Utilize apenas os operadores de soma e subtração para calcular o resultado. Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas de um deles. Assim, 4x5=5+5+5+5=4+4+4+4+4.

---
+++ exercício 5.8.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite o primeiro número do multiplicação: "))
+n2 = int(input("Digite o segundo número da multiplicação: "))
+m = 0
+x = 1
+while x <= n2:
+    m = m + n1
+    x = x + 1
+print ("O resultado da multiplicação é: %d. " % (m))
